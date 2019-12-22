5be88cf90277dc17e158c4370f928b881d7721c3
Anime Lover <bawagupta69@gmail.com>
2019-12-22 23:12:59 +0530

    Basic Physics Applied

---
+++ Assets/Scripts/Trials/MovePaddle.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePaddle : MonoBehaviour
+{
+    [SerializeField] double x = 7.5;
+    [SerializeField] double y = 0.3;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.LeftArrow))
+            x = x-0.1;
+
+        else if(Input.GetKeyDown(KeyCode.RightArrow))
+            x = x+0.1;    
+    }
+}
