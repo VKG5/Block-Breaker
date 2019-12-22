f659db448da4060991893a31bd489f3e81980c91
Anime Lover <bawagupta69@gmail.com>
2019-12-22 19:26:59 +0530

    Basic UI

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,19 @@
+﻿using System.Net.Mime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadNextScene()
+    {
+        int n = SceneManager.sceneCountInBuildSettings;
+        int currSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene((currSceneIndex + 1) % n);    
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
