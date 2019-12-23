b3841148f72c48b84f7bf8129c341379cff17e98
Anime Lover <bawagupta69@gmail.com>
2019-12-23 15:36:07 +0530

    Basic Paddle Functionality

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    [SerializeField] float WidthInUnits = 16f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Getting the mouse position according to the current screen size
+        float mousePositionX = Input.mousePosition.x / Screen.width * WidthInUnits;
+        
+        Vector2 PaddlePos = new Vector2(mousePositionX, transform.position.y);
+        transform.position = PaddlePos;
+    }
+}
