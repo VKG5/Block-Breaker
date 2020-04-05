8f26c6159c62e05e3be3b8cd40e4eed712c06763
Lumi_V <bawagupta69@gmail.com>
2020-04-05 20:54:28 +0530

    New Controls (WASD Proper)

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -5,21 +5,27 @@ using UnityEngine;
 
 public class Paddle : MonoBehaviour
 {
-    private Rigidbody2D rb;
+    [SerializeField] float yPosition = 0.25f;
+    [SerializeField] float xPosition = 11.5f;
+    [SerializeField] float xChange = 0.25f;
+    Vector2 paddlePos;
 
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        paddlePos = new Vector2(xPosition, yPosition);
+        transform.position = paddlePos;
     }
 
     // Update is called once per frame
     void Update()
     {
         if(Input.GetKey(KeyCode.A))
-            rb.velocity = new Vector2(-5f, 0);
+            paddlePos = new Vector2(Mathf.Clamp(transform.position.x-xChange, 1, 20.34f), yPosition);
 
         else if(Input.GetKey(KeyCode.D))
-            rb.velocity = new Vector2(5f, 0);
+            paddlePos = new Vector2(Mathf.Clamp(transform.position.x+xChange, 1, 20.34f), yPosition);
+
+        transform.position = paddlePos;
     }
 }
