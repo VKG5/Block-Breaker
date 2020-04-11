d0b7eadc4287ce5e0e54167134a2e5aaf05ccdcf
Lumi_V <bawagupta69@gmail.com>
2020-04-11 21:49:44 +0530

    Fully Functional Game

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -5,11 +5,30 @@ using UnityEngine;
 public class Block : MonoBehaviour
 {
     [SerializeField] AudioClip blockSound;
+    
+    // Cached reference
+    Level level;
+
+    private void Start() 
+    {
+        // Or you can just type - [SerializeField] Level level; へ
+        // Then dragged and dropped the object in the inspector |
+        level = FindObjectOfType<Level>();    
+
+        // Calling the method to count the number of breakable objects
+        level.countBreakableBlocks();
+    }
 
     private void OnCollisionEnter2D(Collision2D collision) 
+    {
+        Destroy();
+    }
+
+    private void Destroy()
     {
         // Because this is a 3d effect, Camera.main, to access Main Camera
         AudioSource.PlayClipAtPoint(blockSound, Camera.main.transform.position);
         Destroy(gameObject);
+        level.blockDestroyed();
     }
 }
---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour
+{
+    // Parameters
+    // Serialize for debugging purposes
+    [SerializeField] int breakableBlocks;
+    
+    // Cache reference
+    SceneLoader sceneLoader;
+
+    public void Start() 
+    {
+        sceneLoader = FindObjectOfType<SceneLoader>();
+    }
+    
+    public void countBreakableBlocks()
+    {
+        breakableBlocks++;
+    }
+
+    public void blockDestroyed()
+    {
+        breakableBlocks--;
+
+        if (breakableBlocks<=0)
+        {
+            sceneLoader.LoadNextScene();
+        }
+    }
+}
